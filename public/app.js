1bd530dee5852dae4f1b57c11889f701b7b33bf7
Max <max@Maxs-MacBook-Pro.local>
2020-07-01 11:29:30 +0200

    Database added + data handling

--- public/app.js
+++ public/app.js
@@ -1,15 +1,60 @@
-const generate = document.querySelector('#generate');
-generate.addEventListener('click', async (event) => {
+
+const storeForm = document.querySelector('.form-input');
+
+storeForm.addEventListener('submit', (event) => {
+    
     event.preventDefault();
+    const formElements = new FormData(storeForm);
 
-    console.log('Button submitted');
+    const storeName = formElements.get('store-name');
+    const api_key = formElements.get('api-key');
+    const api_pass = formElements.get('api-pass');
+    const gSheet = formElements.get('google-link');
 
+    const storeData = {
+        storeName,
+        api_key,
+        api_pass,
+        gSheet,
+    }
 
-    const response = await fetch('/app/orders')
-    const data = await response.json();
+    sendData(storeData);
 
-    const fullData = data.orders;
-    console.log(fullData);
-
+    console.log(storeData);
 })
+
+async function sendData(data) {
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    }
+    const response = await fetch('/api/send/store-data', options);
+    const answer = await response.json();
+    console.log(answer);
+    
+}
+
+
+
+
+
+
+// const generate = document.querySelector('#generate');
+// generate.addEventListener('click', async (event) => {
+//     event.preventDefault();
+
+//     console.log('Button submitted');
+
+
+//     const response = await fetch('/app/orders')
+//     const data = await response.json();
+
+//     const fullData = data.orders;
+//     console.log(fullData);
+
+// })
 
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 const cors = require('cors');
 const request = require('request-promise');
@@ -12,35 +13,63 @@ app.use(express.static('../public'));
 const API_ENDPOINT = 'https://14921cb6e3f194f30bea0a165d9ef2fd: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@leloyerdesrois.myshopify.com/admin/api/2020-04/orders.json?fields=created_at,line_items,name,total_price,financial_status,product_id'
 
 
-app.get('/app/orders', (req, res) => {
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.post('/api/send/store-data', (req, res) => {
     console.log('I have a request');
 
-    const options = {
-        method: 'GET',
-        uri: API_ENDPOINT,
-        json: true,
-        headers: { 
-            'Content-Type': 'application/json',
+    const data = req.body;
+
+    if (isDataValid(data)) {
+        console.log('Content is valid');
+        const storeData = {
+            storeName: data.storeName.toString(),
+            api_key: data.api_key.toString(),
+            api_pass: data.api_pass.toString(),
+            gSheet: data.gSheet.toString(),
         }
-    };
-    
-    request(options)
-        .then(function(response) {
-            console.log(response);
-            res.status(200);
-            res.json(response);
-            sendDatas(response);
+        database.insert(storeData);
 
-        })
-        .catch(function (err) {
-            console.log(err);
-            res.status(500)
-
-        })
-
+        console.log(storeData);
+    }
+    else {
+        console.error('Content is not valid');
+    }
 })
 
-function sendDatas(data) {
-
-
+function isDataValid(data) {
+    return data.storeName && data.storeName.toString().trim() !== '' &&
+        data.api_key && data.api_key.toString().trim() !== '' &&
+        data.api_pass && data.api_pass.toString().trim() !== '' &&
+        data.gSheet && data.gSheet.toString().trim() !== '';
 }
+
+
+// app.get('/app/orders', (req, res) => {
+
+//     console.log('I have a request');
+
+//     const options = {
+//         method: 'GET',
+//         uri: API_ENDPOINT,
+//         json: true,
+//         headers: { 
+//             'Content-Type': 'application/json',
+//         }
+//     };
+
+//     request(options)
+//         .then(function(response) {
+//             console.log(response);
+//             res.status(200);
+//             res.json(response);
+
+//         })
+//         .catch(function (err) {
+//             console.log(err);
+//             res.status(500)
+//         })
+
+// })
+
